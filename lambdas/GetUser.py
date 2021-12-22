305d63fc1effb80bee9f87a44ae6481f06d7d598
Aahbree <67740010+Aahbree@users.noreply.github.com>
2021-12-22 18:01:32 -0500

    Update GetUser.py

--- lambdas/GetUser.py
+++ lambdas/GetUser.py
@@ -1,6 +1,10 @@
 import json
 import boto3
 import requests
+from elasticsearch import Elasticsearch
+
+esurl="https://search-fantastock0-23vzis4fs4cnwijmzo5e46qpza.us-west-2.es.amazonaws.com"
+es = Elasticsearch([esurl], http_auth=('admin', 'Admin@123'))
 
 def lambda_handler(event, context):
     print(event)
@@ -10,10 +14,21 @@ def lambda_handler(event, context):
     response = client.get_user(
     AccessToken=accesstoken
     )
+    
     print(response)
     name = response['Username']
     print(name)
-    return {
+    for attr in response['UserAttributes']:
+        if attr['Name'] == 'sub':
+            uid= attr['Value']
+    
+    query1 = {"query": {"match": {"_id": uid}}}
+    es_result_user=es.search(index="user", body=query1)
+    current_body = es_result_user['hits']['hits'][0]['_source']
+    print(current_body)
+    return{
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+        'headers': {"Access-Control-Allow-Origin":"*"},
+        'body':current_body,
+        'isBase64Encoded': False
+        }
