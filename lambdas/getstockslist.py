6440167f43f58a26d86811c06a4cf62e4b6bfce2
Pavan Varma <bpavanvarma17@gmail.com>
2021-12-23 18:20:07 -0500

    Update getstockslist.py

--- lambdas/getstockslist.py
+++ lambdas/getstockslist.py
@@ -1,54 +1,32 @@
 
 import pymongo
-
+import json
 
 def lambda_handler(event, context):
     
-    # competition=event["params"]["path"]["competition"]
-    
-    # host="search-fantastock0-23vzis4fs4cnwijmzo5e46qpza.us-west-2.es.amazonaws.com"
-    # awsauth = AWSRequestsAuth(aws_access_key='AKIAWEJK7I4J2XW4AGFS',
-    #                   aws_secret_access_key='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',
-    #                   aws_host=host,
-    #                   aws_region='us-west-2',
-    #                   aws_service='es')
-    
-    # esClient = Elasticsearch(
-    #     hosts=[{'host': host, 'port':443}],
-    #     use_ssl=True,
-    #     http_auth=awsauth,
-    #     verify_certs=True,
-    #     connection_class=RequestsHttpConnection)
-    
-    # searchData = esClient.search({"query": {"match": {"competition": competition}}})
+    sectors = dict({'DecMonthlyCompetition': 'Financial Services', 'WEEK: 43': 'Healthcare', '2022-MEGA': 'Technology','WeeklyMini': 'Healthcare', "Demo Competition":"Healthcare"})
 
-    # for competition in searchData['hits']['hits']:
-    #     competitions.append(competition['_source'])
-
-    
-    # if not competitions:
-    #     return{
-    #         'statusCode':200,
-    #         "headers": {"Access-Control-Allow-Origin":"*"},
-    #         'body': json.dumps('No Competitions found')
-    #     }
-    # else:    
-    #     return{
-    #         'statusCode': 200,
-    #         'headers': {"Access-Control-Allow-Origin":"*"},
-    #         'body': competitions,
-    #         'isBase64Encoded': False
-    #     }
-        
-        
     competition=event['body-json']['competition']
-    
+    #e=json.loads(event)
+    sector=""
+    if 'sector' in event.keys():
+        if event['sector']:
+            sector=event['sector']
+
+    if not sector : 
+        sector = "Technology"
+    if competition and competition in sectors.keys():
+        sector =sectors[competition]
     client = pymongo.MongoClient("mongodb+srv://admin:admin123@fantastock.h4npu.mongodb.net/fanta_stock?retryWrites=true&w=majority")
     db = client.fanta_stock
     collection=db.stockdata
-    stockslist=collection.distinct('name')
+    myquery = {'sector':sector}
+    mydoc = collection.find(myquery)
+    stockslist=[]
     
-    print(type(stockslist))
+    for stocks in mydoc:
+        if 'name' in stocks.keys() and stocks['name'] and stocks['name'].lower() != 'null' :
+            stockslist.append(stocks['name'])
     
     if not stockslist:
         return{
@@ -57,6 +35,7 @@ def lambda_handler(event, context):
             'body': json.dumps('No Companies found')
         }
     else:    
+        stockslist.sort()
         return{
             'statusCode': 200,
             'headers': {"Access-Control-Allow-Origin":"*"},
